0f62ebdf30c98b019cbb665a92edfa793d5894ad
firasmar3bi <mfiras190@gmail.com>
2024-08-19 18:29:31 +0300

    Create Project

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCFirstTask.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public IActionResult Update()
+        {
+            return View("Update");
+        }
+        public IActionResult Delete()
+        {
+            return View("Delete");
+        }
+
+    }
+}
+
